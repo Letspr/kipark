e24184cb1365722373665c1e36257d6960e87aea
Letspr <lparrondo@gmail.com>
2022-06-12 19:58:49 +0200

    primer commit

---
+++ android/app/src/main/kotlin/com/example/kipark/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kipark
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ejercicio 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text(
+        'Hola mundo',
+        style: TextStyle(color: Colors.deepPurple, fontSize: 40.0),
+      ),
+    );
+  }
+}
